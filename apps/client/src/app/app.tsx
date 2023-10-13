eb7f25916b047b5e5032da1dd45c66b8613a89ce
Pepe González <pegondo99@gmail.com>
2023-10-13 19:36:34 +0200

    Add simple client and server.

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -1,14 +1,73 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
+import React, { ChangeEvent, useState, useEffect } from 'react';
+import useWebSocket, { ReadyState } from 'react-use-websocket';
 
-import NxWelcome from './nx-welcome';
+const SOCKER_URL = 'ws://localhost:8000/upgrade';
+const SOCKET_CONNECTION_STATUS_MAPPING = {
+  [ReadyState.CONNECTING]: 'Connecting',
+  [ReadyState.OPEN]: 'Open',
+  [ReadyState.CLOSING]: 'Closing',
+  [ReadyState.CLOSED]: 'Closed',
+  [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
+};
 
-export function App() {
+const App = () => {
+  const [message, setMessage] = useState('');
+  const { sendMessage, lastMessage, readyState } =
+    useWebSocket<string>(SOCKER_URL);
+  const [messageHistory, setMessageHistory] = useState<MessageEvent<string>[]>(
+    []
+  );
+  const connectionStatus = SOCKET_CONNECTION_STATUS_MAPPING[readyState];
+
+  const handleMsgChange = ({
+    target: { value },
+  }: ChangeEvent<HTMLTextAreaElement>) => {
+    setMessage(value);
+  };
+
+  const onClick = () => {
+    sendMessage(message);
+  };
+
+  // Update the history.
+  useEffect(() => {
+    if (!lastMessage) return;
+    setMessageHistory((prev) => prev.concat(lastMessage));
+  }, [lastMessage]);
+
   return (
     <div>
-      <NxWelcome title="client" />
+      <div>
+        <pre>Connection status: {connectionStatus}</pre>
+      </div>
+      <div>
+        <textarea
+          rows={4}
+          cols={50}
+          onChange={handleMsgChange}
+          value={message}
+        />
+      </div>
+      <div>
+        <button onClick={onClick} disabled={readyState !== ReadyState.OPEN}>
+          Send
+        </button>
+      </div>
+      <div>
+        <pre>
+          Last message: {lastMessage?.data || 'There are no new messages'}
+        </pre>
+        <pre>
+          Message history:{' '}
+          {JSON.stringify(
+            messageHistory.map((message) => message.data).reverse(),
+            null,
+            4
+          )}
+        </pre>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,6 +1,18 @@
-import { serve } from 'https://deno.land/std@0.181.0/http/server.ts';
-import { handler } from './handler.ts';
+const sleep = (milliseconds: number) =>
+  new Promise((resolve) => setTimeout(resolve, milliseconds));
 
-const port = Number(Deno.env.get('PORT') || 4200);
-
-await serve(handler, { port });
+Deno.serve((req) => {
+  if (req.headers.get('upgrade') !== 'websocket') {
+    return new Response(null, { status: 501 });
+  }
+  const { socket, response } = Deno.upgradeWebSocket(req);
+  socket.addEventListener('open', () => {
+    console.log('a client connected!');
+  });
+  socket.addEventListener('message', async (event) => {
+    socket.send(`${event.data} - Received!`); // Send message with its own message.
+    await sleep(3000); // Wait 3 seconds.
+    socket.send('Three seconds later...'); // Send one more message.
+  });
+  return response;
+});
