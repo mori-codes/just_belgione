53e2a762373158d94830014d6cda1906bed3af09
mori-codes <mori.codes.oficial@gmail.com>
2023-10-17 21:23:32 +0200

    Add PWA to the app

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig(({ mode }) => {
   return {
@@ -29,7 +30,46 @@ export default defineConfig(({ mode }) => {
       },
     },
 
-    plugins: [react(), nxViteTsPaths()],
+    plugins: [
+      react(),
+      nxViteTsPaths(),
+      VitePWA({
+        registerType: 'autoUpdate',
+        devOptions: {
+          enabled: mode === 'development',
+        },
+        manifest:{
+          name: "Solo Una",
+          short_name: "Solo Una",
+          description: "By p&m",
+          start_url: "/index.html",
+          display: "standalone",
+          lang: "es",
+          display_override: ["standalone", "window-controls-overlay"],
+          orientation: "portrait",
+          dir: "ltr",
+          icons: [
+            {
+              src: "/favicon-32x32.png",
+              sizes: "32x32",
+              type: "image/x-icon",
+            },
+            {
+              src: "/android-chrome-192x192.png",
+              sizes: "192x192",
+              type: "image/png",
+              purpose: "any",
+            },
+            {
+              src: "/android-chrome-192x192.png",
+              sizes: "512x512",
+              type: "image/png",
+              purpose: "any",
+            },
+          ],
+        }
+      }),
+    ],
 
     // Uncomment this if you are using workers.
     // worker: {
