c123c0f844f7c7626f15abb837e03e9230bfdfa1
mori-codes <mori.codes.oficial@gmail.com>
2023-11-21 20:35:13 +0100

    Styles for the player guessing waiting screen

--- apps/client/src/app/Room/Game/PlayerGuessing/PlayerGuessing.tsx
+++ apps/client/src/app/Room/Game/PlayerGuessing/PlayerGuessing.tsx
@@ -1,15 +1,18 @@
-import { ServerMessage } from "@just-belgione/types";
+import { ServerMessage } from '@just-belgione/types';
+import { PageWrapper } from '../../../components/common/PageWrapper';
 
 type Props = {
   lastJsonMessage: ServerMessage;
-}
+};
 
 const PlayerGuessing: React.FC<Props> = () => {
   return (
-    <div>
-      <h2>¡Es tu turno!</h2>
-      <p>Tus compañeros están pensando cuál es la mejor pista.</p>
-    </div>
+    <PageWrapper variant="dark" center>
+      <div className='flex min-h-[100dvh] justify-center text-white flex-col text-center gap-4 drop-shadow-lg px-4'>
+        <h2 className='text-jo-lg'>¡Es tu turno!</h2>
+        <p className='text-jo-md'>Tus compañeros están pensando cuál es la mejor pista</p>
+      </div>
+    </PageWrapper>
   );
 };
 
