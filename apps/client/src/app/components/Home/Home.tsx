6a58caedb94b78613d75969905a7b261044b2a5f
Javi <105231457+mori-codes@users.noreply.github.com>
2023-12-27 09:43:41 +0100

    🚈

--- apps/client/src/app/components/Home/Home.tsx
+++ apps/client/src/app/components/Home/Home.tsx
@@ -29,7 +29,7 @@ const Home = () => {
         <img src="/logoimage.png" alt="Solo una" />
         <div className="flex flex-col gap-12 pt-8">
           <Input
-            label="Introduce tu nombre"
+            label="🚈 Introduce tu nombre"
             value={user}
             onChange={(e) => setUser(e.target.value)}
           />
